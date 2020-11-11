4e36233c30fa574866305412e6b288b11e6ceb53
ch-hassansaeed <ch.hassansaeed@gmail.com>
2020-11-11 19:30:31 +0500

    Add files via upload

---
+++ ConsoleAppHelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Good Bye");
+            //Console.ReadLine();//now it will close after enter press
+            //or you can run without debug like
+            //thanks for watching good luck for c# learning :)
+        }
+    }
+}
